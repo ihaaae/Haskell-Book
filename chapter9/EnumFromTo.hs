3a97d2d0fb4bed6da67335ec42692c6566f57998
liu <5929624d@gmail.com>
2023-03-10 16:31:39 +0800

    answers of homework from haskell book

--- chapter9/EnumFromTo.hs
+++ chapter9/EnumFromTo.hs
@@ -1,6 +1,21 @@
 module EnumFromTo where
 
-myEnumFromTo :: Enum a => a -> a -> [a]
-myEnumFromTo start end 
-    | fromEnum start > fromEnum end = []
-    | otherwise    = start : myEnumFromTo (succ start) end
+eftBool :: Bool -> Bool -> [Bool]
+eftBool start end
+    | start > end = []
+    | otherwise = start : eftBool (succ start) end
+
+eftOrd :: Ordering -> Ordering -> [Ordering]
+eftOrd start end
+    | start > end = []
+    | otherwise = start : eftOrd (succ start) end
+
+eftInt :: Int -> Int -> [Int]
+eftInt start end
+    | start > end = []
+    | otherwise = start : eftInt (succ start) end
+
+eftChar :: Char -> Char -> [Char]
+eftChar start end
+    | start > end = []
+    | otherwise = start : eftChar (succ start) end
